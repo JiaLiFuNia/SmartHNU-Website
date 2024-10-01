be3d9aa8ff0048a4afd0015637ef38f1c61b2d8f
Jialifuniya <woshixubohan04@163.com>
2024-10-01 15:57:08 +0800

    update

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,6 +1,6 @@
 export function onRequest(context) {
   const jsonResponse = Response.json(
-    { nmae: "SmartHNU", version: "Release-2.1.8.1" },
+    { name: "SmartHNU", version: "Release-2.1.8.1" },
   );
   logResponse(jsonResponse);
 }
