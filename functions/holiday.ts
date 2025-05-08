320bcf46ea1e937ae2fbfd7a03de63c56f98ab37
Jialifuniya <woshixubohan04@163.com>
2025-05-08 13:25:55 +0800

    update

--- functions/holiday.ts
+++ functions/holiday.ts
@@ -1,3 +1,8 @@
 export function onRequest(context) {
-    
+    return new Response(JSON.stringify(""), {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        }
+      });
 }
