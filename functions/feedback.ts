7f3948891ec9c8b2e9f8619b8edc2d77f9d74483
Jialifuniya <woshixubohan04@163.com>
2025-05-10 11:21:41 +0800

    fix: 修正反馈函数中的变量名以保持一致性

--- functions/feedback.ts
+++ functions/feedback.ts
@@ -1,5 +1,5 @@
 export async function onRequestPost(context) {
-    const { type, functionModel, message, email, androidVersion, system, device, versionCode } = await context.request.json();
+    const { type, functionModule, message, email, androidVersion, system, device, versionCode } = await context.request.json();
     const api_key = context.env.RESENT;
     if (!message) {
         return new Response(JSON.stringify({ success: false, message: "反馈不能为空" }), {
@@ -18,7 +18,7 @@ export async function onRequestPost(context) {
             from: "SmartHNU_Feedback@resend.dev",
             to: "xbh0704@outlook.com",
             subject: type,
-            html: `<p><strong>功能模块:</strong> ${functionModel}</p>
+            html: `<p><strong>功能模块:</strong> ${functionModule}</p>
                <p><strong>邮箱:</strong> ${email}</p>
                 <p><strong>设备:</strong> Android ${androidVersion}；${system}；${device}</p>
                 <p><strong>软件版本:</strong> ${versionCode}</p>
