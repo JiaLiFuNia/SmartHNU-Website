e00499a4622d51349a7195c70f8097e73baeb261
Jialifuniya <woshixubohan04@163.com>
2025-05-11 11:41:32 +0800

    fix: 修正最新版本检查逻辑以确保正确比较版本号

--- functions/latest.ts
+++ functions/latest.ts
@@ -7,7 +7,7 @@ export async function onRequestPost(context) {
     }
 
     const latestVersion = "3.0.9"
-    const latestVersionCode = 202505071
+    const latestVersionCode = 202505112
     const isForceUpdate = false // 是否强制更新
     const data = {
         "code": 200,
@@ -19,7 +19,7 @@ export async function onRequestPost(context) {
             "isForceUpdate": isForceUpdate,
             "update": {
                 "url": "",
-                "content": "更新内容\n- 修复已知问题\n- 优化用户体验\n- 提升性能",
+                "content": "更新内容\n-新增 反馈页面\n-新增 节假日信息获取\n- 新增 忘记密码提示弹窗\n- 新增 清除 Cookie 和 WebView 数据\n- 优化 更新检查体验",
             }
         }
     }
