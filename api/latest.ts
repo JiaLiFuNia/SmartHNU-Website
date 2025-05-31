9ca2559b314a73773623c817ec4772d1ec5a9fd8
Jialifuniya <woshixubohan04@163.com>
2025-05-31 10:50:27 +0800

    fix: 更新最新版本号和版本代码

--- api/latest.ts
+++ api/latest.ts
@@ -17,14 +17,14 @@ export default async function handler(
   }
 
   const latestVersion = "3.0.9";
-  const latestVersionCode = 202505302;
+  const latestVersionCode = 202505312;
   const isForceUpdate = false; // 是否强制更新
   
   const isNeedUpdate = currentVersionCode < latestVersionCode;
   const update = {
     "downloadUrl": "https://xubohan04.tk/SmartHNU_v3.0.9(202505301).apk",
     "content": "更新内容\n- 新增 反馈页面\n- 新增 节假日信息获取\n- 新增 忘记密码提示弹窗\n- 新增 清除 Cookie 和 WebView 数据\n- 优化 更新检查体验",
-  }
+  };
 
   const data = {
     "message": "success",
