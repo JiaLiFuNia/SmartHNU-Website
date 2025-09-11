8d182e6c08e6c0ee779eee6cc78cdd55cbf21b65
Jialifuniya <woshixubohan04@163.com>
2025-09-12 00:14:48 +0800

    fix: 更新版本号和下载链接，调整更新内容

--- api/latest.ts
+++ api/latest.ts
@@ -17,14 +17,14 @@ export default async function handler(
     currentVersionCode = requestData.versionCode;
   }
 
-  const latestVersion = "3.0.9";
-  const latestVersionCode = 202509081;
+  const latestVersion = "3.0.9.1";
+  const latestVersionCode = 202509111;
   const isForceUpdate = false; // 是否强制更新
   
   const isNeedUpdate = currentVersionCode < latestVersionCode;
   const update = {
-    "downloadUrl": "https://xubohan04.tk/SmartHNU_v3.0.9(202509081).apk",
-    "content": "更新内容\n注意：更新该版本后，需要清楚软件数据。\n-新增 支持修改新闻页面字体样式\n- 优化 部分页面显示",
+    "downloadUrl": "https://xubohan04.tk/SmartHNU_v3.0.9.1(202509111).apk",
+    "content": "更新内容\n- 新增 统一认证系统登录逻辑\n- 新增 空闲教室可查询增加更多教室\n- 修复 课表日期显示错误的问题",
   };
 
   const data = {
